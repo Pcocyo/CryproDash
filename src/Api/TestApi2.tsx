5a39140c131b1d04d8014eea8255ea8945df9a55
Pcocyo <joshuaelson46@gmail.com>
2024-11-12 14:00:16 +0800

    initial-files

---
+++ src/Api/TestApi2.tsx
@@ -0,0 +1,18 @@
+// api filter for categories
+// https://coingecko.com/en/categories
+
+//api for full market 
+//https://docs.coingecko.com/v3.0.1/reference/coins-markets
+//https://api.coingecko.com/api/v3/coins/markets
+
+// api for trending search list
+// https://api.coingecko.com/api/v3/search/trending
+
+//api for Coin data by id
+//https://docs.coingecko.com/v3.0.1/reference/coins/{id}
+// https://docs.coingecko.com/v3.0.1/reference/coins-id
+
+
+// api for historical data
+// https://api.coingecko.com/api/v3/coins/{id}/market_chart
+// Coin Historical Chart Data by ID
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
-const App = () => {
+// import Contact1 from './test_files/Contact1'
+// import Test from './test_files/Recharts/Test'
+// import Tests2 from './test_files/Tests2'
+// import Layout from './test_files/Layout'
+import {bitcoin_historical_data} from './Api/TestApi2'
+const App: React.FC = () => {
+  console.log(bitcoin_historical_data)
   return (
-    <div>App</div>
+    <div>
+      test
+    </div>
   )
 }
 
